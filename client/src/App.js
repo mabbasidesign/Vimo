976d4c9361c08bfe0c03dda0bfb3cbe7920027d7
unknown <mabbasidesign@yahoo.com>
2019-01-21 21:46:22 +0330

    update packages

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class App extends Component {
+
+componentDidMount = () => {
+  axios.get('/api/product/brands').then(response => {
+      console.log(response);
+  })
+}
+
   render() {
     return (
       <div>
