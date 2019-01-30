372d50d9ab7148f437bc45698079e576fe0397c1
unknown <mabbasidesign@yahoo.com>
2019-01-30 10:55:38 +0330

    add product action, reducer

--- client/src/component/home/index.jsx
+++ client/src/component/home/index.jsx
@@ -2,8 +2,15 @@ import React, { Component } from 'react';
 import HomeSlider from './home_slider';
 import HomePromotion from './home_promotions';
 
+import { connect } from 'react-redux';
+import { getProductsBySell, getProductsByArrival } from '../../actions/products_actions';
+
 class Home extends Component {
-    state = {  }
+    
+    componentDidMount(){
+        this.props.dispatch(getProductsBySell());
+        this.props.dispatch(getProductsByArrival());
+    }
 
     render() { 
         return (
@@ -14,5 +21,11 @@ class Home extends Component {
         );
     }
 }
- 
-export default Home;
+
+const mapStateToProps = (state) => {
+    return {
+        products: state.products
+    }
+}
+
+export default connect(mapStateToProps) (Home);
