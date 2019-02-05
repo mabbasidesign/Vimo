c1b7b2a31ba901aa9263ce9d5a11ce5c9d7a9a1e
unknown <mabbasidesign@yahoo.com>
2019-02-05 14:31:33 +0330

    add create admin add product

---
+++ client/src/component/user/Admin/add_products.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class AddProducts extends Component {
+
+    state = {  }
+
+    render() { 
+        return (
+            <div>
+                add products
+            </div>
+        );
+    }
+}
+ 
+export default AddProducts;
--- client/src/routes.js
+++ client/src/routes.js
@@ -9,12 +9,15 @@ import RegisterLogin from './component/Register_login/index';
 import Register from './component/Register_login/Register';
 import userDashboard from './component/user/index';
 import Shop from './component/shop/index';
+import AddProduct from './component/user/Admin/add_products';
 
 const Routes = () => {
   return (
     <Layout>
       <Switch>
-        <Route path='/user/dashboard' exact component={Auth(userDashboard, true)}/>
+        <Route path='/user/dashboard' exact component={Auth(AddProduct, true)}/>
+        <Route path='/admin/add_products' exact component={Auth(userDashboard, true)}/>
+
         <Route path='/register' exact component={Auth(Register, false)}/>
         <Route path='/register_login'exact component={Auth(RegisterLogin, false)}/>
         <Route path='/shop' exact component={Auth(Shop, false)}/>
