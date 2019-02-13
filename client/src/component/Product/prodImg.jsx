0846d1fa6d4149cc80952c6c558980cb493c7f53
unknown <mabbasidesign@yahoo.com>
2019-02-13 21:53:37 +0330

    add images to product detail

--- client/src/component/Product/prodImg.jsx
+++ client/src/component/Product/prodImg.jsx
@@ -21,12 +21,63 @@ class ProdImg extends Component {
         }
     }
 
+
+    renderCardImage = (images) => {
+        if(images.length > 0){
+            return images[0].url
+        }else{
+            return `/images/image_not_availble.png`
+        }
+    }
+
+
+    showThumbs = () => (
+        this.state.lightBoxImages.map((item, i) => (
+            i > 0 ?
+                <div 
+                    key={i}
+                    onClick={() => this.handleLightBox(i)}
+                    className='thumb'
+                    style={{background: `url(${item}) no-repeat`}}
+                >
+                </div>
+            : null
+        ))
+    )
+
+
+    // showThumbs = () => (
+    //     this.state.lightboxImages.map((item,i)=>(
+    //         i > 0 ?
+    //             <div
+    //                 key={i}
+    //                 onClick={()=> this.handleLightBox(i)}
+    //                 className="thumb"
+    //                 style={{background: `url(${item}) no-repeat`}}
+    //             ></div>
+    //         :null
+    //     ))
+    // )
+
+
+    handleLightBox = () => {
+        
+    }
+
+
     render() {
-        const {detail} = this.props.detail
+        const {detail} = this.props;
         return (
             <div className='product_image_container'>
                 <div className='main_pic'>
-
+                    <div
+                        style={{background:`url(${this.renderCardImage(detail.images)}) no-repeat`}}
+                        onClick={()=> this.handleLightBox(0)}
+                    >
+                    </div>
+                    <div className="main_thumbs">
+                    { this.showThumbs(detail)}
+                </div>
                 </div>
             </div>
         );
@@ -34,3 +85,4 @@ class ProdImg extends Component {
 }
  
 export default ProdImg;
+
