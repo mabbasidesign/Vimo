6a1e25b7cbd2a079440baa722f3cc29a16b743ad
unknown <mabbasidesign@yahoo.com>
2019-02-19 22:14:29 +0330

    creating cart block - shopping cart

--- client/src/component/user/cart.jsx
+++ client/src/component/user/cart.jsx
@@ -1,5 +1,7 @@
 import React, { Component, useReducer } from 'react';
 import UserLayout from '../../hoc/user';
+import UserProductBlock from '../../utils/User/productBlock';
+
 import { connect } from 'react-redux';
 import { getCartItems } from '../../actions/user_actions';
 
@@ -17,15 +19,15 @@ class UserCard extends Component {
     }
 
     componentDidMount = () => {
-        let carttItem = [];
+        let carttItems = [];
         let user = this.props.user;
 
         if(user.userData.cart){
             if(user.userData.cart.length > 0){
                 user.userData.cart.forEach(item => {
-                    carttItem.push(item.id)
+                    carttItems.push(item.id)
                 });
-                this.props.dispatch(getCartItems(carttItem, user.userData.cart))
+                this.props.dispatch(getCartItems(carttItems, user.userData.cart))
                 .then(() => {
                     
                 })
@@ -33,11 +35,24 @@ class UserCard extends Component {
         }
     }
 
+
+    removeFromCart = () => {
+        
+    }
+
+
     render() { 
         return (
             <UserLayout>
                 <div>
-                    cart
+                    <h1>My Cart</h1>
+                    <div className="user_cart">
+                        <UserProductBlock
+                           products={this.props.user}
+                           type='cart'
+                           removeItem={(id) => this.removeFromCart(id)}
+                        />
+                    </div>
                 </div>
             </UserLayout>
         );
