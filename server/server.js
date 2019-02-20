a3279de7da324c5129a742a4f576c2c023a17b6d
unknown <mabbasidesign@yahoo.com>
2019-02-20 19:10:42 +0330

    add update porofile route

--- server/server.js
+++ server/server.js
@@ -333,6 +333,24 @@ app.get('/api/users/removeFromCart',auth,(req,res)=>{
 })
 
 
+app.post('/api/users/update_profile',auth,(req,res)=>{
+
+    User.findOneAndUpdate(
+        { _id: req.user._id },
+        {
+            "$set": req.body
+        },
+        { new: true },
+        (err,doc)=>{
+            if(err) return res.json({success:false,err});
+            return res.status(200).send({
+                success:true
+            })
+        }
+    );
+})
+
+
 const port = process.env.PORT || 3002;
 app.listen(port,()=>{
     console.log(`Server Running at ${port}`)
